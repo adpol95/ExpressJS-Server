44184f48adc0006801364a8a8b2d7ea2d8f8e409
adpol95 <adpol95@gmail.com>
2023-03-10 20:39:47 +0300

    db update

--- src/modules/core/db.js
+++ src/modules/core/db.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose';
 
+const connectionString =
+  'mongodb+srv://adpol95:MlWkg1a2F5o8FtCX@cluster0.izr2wlk.mongodb.net/expressPASV?retryWrites=true&w=majority';
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 function dbConnect() {
-  mongoose.connect('mongodb://127.0.0.1:27017/express', {
-    useUnifiedTopology: true,
-  });
+  mongoose
+    .connect(connectionString, options)
+    .then(() => console.log('Mongo connection OK'))
+    .catch((err) => console.log(err));
   const db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'connection error:'));
