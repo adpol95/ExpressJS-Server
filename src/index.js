6dfa7adb6cc659b78644da62b244e2fad28a40de
Anton Polegaev <88550196+adpol95@users.noreply.github.com>
2023-03-10 21:05:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 dbConnect(); //Mongoose
 logger(app); // Morgan
 parseResponse(app);
-cors(app); // Не сакционированный переход
+//cors(app); // Не сакционированный переход
 routes(app);
 errorHandler(app);
 
